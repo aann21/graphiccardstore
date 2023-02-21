7fcb424740adf9e3e84aec00af6753f888f44e20
aann21 <anlonglego@gmail.com>
2023-02-21 21:56:47 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+    //banner carousel
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots: true,
+        items: 1,
+        autoplay: true,
+        loop: true
+
+    });
+
+    //top sale carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: false,
+        responsive: {
+            0:{
+                items: 1
+            },
+            600:{
+                items: 3
+            },
+            1000:{
+                items: 5
+            }
+        }
+        
+    });
+
+    //isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+
+    });
+
+    //filter item
+    $(".button-group").on("click","button",function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter: filterValue});
+    });
+
+    //new products carousel
+    $("#new-products .owl-carousel").owlCarousel({
+        loop: true,
+        autoplay:true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        responsive: {
+            0:{
+                items: 1
+            },
+            600:{
+                items: 3
+            },
+            1000:{
+                items: 5
+            }
+        }
+    });
+
+    //news carousel
+    $("#news .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        responsive: {
+            0:{
+                items: 1
+            },
+            600:{
+                items: 3
+            }
+        }
+    });
+
+    //product quantity
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    //let $input = $(".qty .qty_input");
+
+    $qty_up.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() >= 1 && $input.val() <= 9){
+            $input.val(function(i, oldval){
+                return ++oldval;
+            })
+        }
+    });
+
+    $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() > 1 && $input.val() <= 10){
+            $input.val(function(i, oldval){
+                return --oldval;
+            })
+        }
+    });
+
+    //product img-name
+    $('#validatedCustomFile').on('change',function(e){
+        var fileName = e.target.files[0].name;
+        $('.custom-file-label').html(fileName);
+    });
+});
